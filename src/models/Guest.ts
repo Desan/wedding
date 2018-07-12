b69344d71b02eaa762e3f17643e9de474a2814d7
Desan <lizardesan@gmail.com>
2018-07-13 02:02:11 +0300

    mongo model for Guest

---
+++ src/models/Guest.ts
@@ -0,0 +1,58 @@
+import { prop, Typegoose } from 'typegoose'
+
+export class Guest extends Typegoose {
+    /**
+     * Имя гостя
+     */
+    @prop({required: true})
+    name?: string
+  
+    /**
+     * Приял ли приглание
+     */
+    @prop()
+    isAccept?: boolean
+
+    /**
+     * Будет ли произность речь
+     */
+    @prop()
+    hasSpeech?: boolean
+
+    /**
+     * Хочет ли поехать на церемонию
+     */
+    @prop()
+    whantsCeremony?: boolean
+    
+    /**
+     * Выпить
+     */
+    @prop({ enum: Guest.Alcohol })
+    alcohol?: Guest.Alcohol
+
+    /**
+     * Покушать
+     */
+    @prop({ enum: Guest.Food })
+    food?: Guest.Food
+
+}
+
+const GuestModel = new Guest().getModelForClass(Guest)
+
+export namespace Guest {
+    export enum Alcohol {
+        WEAK = 'weak',
+        STRONG = 'strong',
+        ALL = 'all',
+        NONE = 'none'
+    }
+    export enum Food {
+        FISH = 'fish',
+        BEEF = 'beef',
+        BURGER = 'burger',
+        PLANTS = 'plants'
+    }
+    export const Model = GuestModel
+}
