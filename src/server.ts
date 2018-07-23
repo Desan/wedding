dbb1862a7ecea9ee9236bbaf68d19491f08cd632
Desan <lizardesan@gmail.com>
2018-07-24 00:47:51 +0300

    hardcode hostname

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ let PORT = 3000
 if (process.argv.some(arg => arg === "--prod")) {
     PORT = 80
 }
-process.env.HOST = 'http://11-08-2018.ml/'
 
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT)
